2dc96540717ae8411aa9a9a52d5a4f49a7dbc836
Conail Stewart <conailstewart@gmail.com>
2016-03-24 17:25:57 -0400

    Remove linked photo from Home.

--- spa/modules/Home.js
+++ spa/modules/Home.js
@@ -4,7 +4,6 @@ export default React.createClass({
   render() {
     return <div id="home">
       <h1>Home</h1>
-      <img src="https://images.unsplash.com/photo-1421987392252-38a07781c07e?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=43b6196807cf7d9c7a05803c820db8ef" width="100%"/>
     </div>;
   }
 })
